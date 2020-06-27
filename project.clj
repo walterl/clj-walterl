7313e20b80af03af5aab77babbf121a7bbf34866
Walter Leibbrandt <git@wrl.co.za>
2020-06-27 17:26:31 +0200

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clj-walterl "0.1.0-SNAPSHOT"
-  :description "Clojure-y bits and bobs"
+  :description "My Clojure-y bits and bobs"
   :url "https://github.com/walterl/clj-walterl"
   :license {:name "EPL-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
